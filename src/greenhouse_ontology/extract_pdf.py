4ef8037d5ec6d8f47d350199a3c87f29e98870d5
LiamYS <lvlaskamp@gmail.com>
2026-09-13 20:45:14 +0200

    feat: add extract pdf script

---
+++ src/greenhouse_ontology/extract_pdf.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import sys
+import pymupdf4llm
+
+
+def main(input_path: str | Path, output_path: str | Path):
+    text = pymupdf4llm.to_text(input_path)
+    Path(output_path).write_text(text, encoding="utf-8")
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
